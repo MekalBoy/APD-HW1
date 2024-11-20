fa5edf99b3f7e92385e4f74fc2dc98efd65fb4d9
Mekal <23715925+MekalBoy@users.noreply.github.com>
2024-11-20 19:44:40 +0200

    Update .gitignore, main.c
    There is now an even distribution of files between mappers, though it's based solely on the number of files. It's not too efficient.

--- src/main.c
+++ src/main.c
@@ -1,15 +1,35 @@
 #include <pthread.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_BUFFER 64 // How big can a line be anyway?
 
 struct args {
     int thread_id;
-    pthread_barrier_t *mapstop;
+    pthread_barrier_t* mapstop; // Barrier that everyone syncs to
+    int nr_files; // Number of files a mapper will handle
+    struct fileinfo* files; // The files a mapper will process
 };
+
+struct fileinfo {
+    char fileName[MAX_BUFFER];
+    int id;
+    FILE* file;
+};
+
+void printArgs(struct args myargs) {
+    printf("thread_id %d; nr_files %d\n", myargs.thread_id, myargs.nr_files);
+    if (myargs.nr_files > 0) {
+        for (int i = 0; i < myargs.nr_files; i++) {
+            printf("file %d: id %d; filename %s;\n", i, myargs.files[i].id, myargs.files[i].fileName);
+        }
+    }
+}
 
 unsigned long fsize(char* file)
 {
-    FILE * f = fopen(file, "r");
+    FILE *f = fopen(file, "r");
     fseek(f, 0, SEEK_END);
     unsigned long len = (unsigned long) ftell(f);
     fclose(f);
@@ -21,6 +41,23 @@ void *mapper(void *arg) {
     struct args myargs = *(struct args *)arg;
 
     printf("%d Mapper started\n", myargs.thread_id);
+
+    printf("%d Mapper has %d files.\n", myargs.thread_id, myargs.nr_files);
+    printArgs(myargs);
+
+    char wordBuffer[MAX_BUFFER];
+    if (myargs.nr_files > 0) { 
+        for (int i = 0; i < myargs.nr_files; i++) {
+            myargs.files[i].file = fopen(myargs.files[i].fileName, "r");
+
+            while (fscanf(myargs.files[i].file, "%s\n", wordBuffer) == 1) {
+                printf("%s ", wordBuffer);
+            }
+            printf("\n");
+
+            fclose(myargs.files[i].file);
+        }
+    }
 
     pthread_barrier_wait(myargs.mapstop);
     return 0;
@@ -77,13 +114,31 @@ int main(int argc, char **argv)
         printf("Inputs: %d %d %s\n", nr_mappers, nr_reducers, argv[3]);
     }
 
+    int NUM_THREADS = nr_mappers + nr_reducers;
+
+    pthread_barrier_t mapstop;
+    pthread_barrier_init(&mapstop, NULL, NUM_THREADS);
+
+    // Compose arguments
+
+    pthread_t threads[NUM_THREADS];
+	struct args arguments[NUM_THREADS];
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        arguments[i].nr_files = 0;
+        arguments[i].files = NULL;
+
+        // fix this pls
+        arguments[i].files = malloc(sizeof(struct fileinfo) * nr_files);
+    }
+
+
     int i, r;
-    char lineBuffer[64]; // How big can a line be anyway?
+    char lineBuffer[MAX_BUFFER];
     for (i = 0; i < nr_files; i++) {
-        int r;
         r = fscanf(input_file, "%s\n", lineBuffer);
 
-        if (r == -1) {
+        if (r == EOF) {
             printf("Tried to read another line, but there are no more lines.\n");
             exit(1);
         }
@@ -91,17 +146,23 @@ int main(int argc, char **argv)
         if (debug) {
             printf("File %s has size %lu.\n", lineBuffer, fsize(lineBuffer));
         }
+
+        struct fileinfo newFile;
+        strcpy(newFile.fileName, lineBuffer);
+        // newFile.file = fopen(lineBuffer, "r");
+        newFile.id = i;
+
+        // Even distribution, kinda bad -- should be done based on fsize
+        int lucky_mapper = i % nr_mappers;
+        arguments[lucky_mapper].files[arguments[lucky_mapper].nr_files] = newFile;
+        arguments[lucky_mapper].nr_files++;
     }
 
+    // arguments[0].files[0].id = 1;
+    // strcpy(arguments[0].files[0].fileName, "hehe");
+    // printArgs(arguments[0]);
+
     // Initialize threads
-
-    int NUM_THREADS = nr_mappers + nr_reducers;
-
-    pthread_barrier_t mapstop;
-    pthread_barrier_init(&mapstop, NULL, NUM_THREADS);
-    
-    pthread_t threads[NUM_THREADS];
-	struct args arguments[NUM_THREADS];
 
     // Note for self: last thread will be (NUM_THREADS - 1)
 	for (i = 0; i < NUM_THREADS; i++) {
@@ -125,6 +186,8 @@ int main(int argc, char **argv)
 		}
 	}
 
+    // Await threads
+
     void *status;
     for (i = 0; i < NUM_THREADS; i++) {
 		r = pthread_join(threads[i], &status);
@@ -139,6 +202,8 @@ int main(int argc, char **argv)
 			exit(-1);
 		}
 	}
+
+    // Wrap-up (free & close)
 
     pthread_barrier_destroy(&mapstop);
     fclose(input_file);
