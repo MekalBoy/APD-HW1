e76f686796c026a5e1ff30f4067c32cd84e36111
Mekal <23715925+MekalBoy@users.noreply.github.com>
2024-11-23 14:30:03 +0200

    Implement mapper load balancing
    Mappers now receive evenly sized files to process.
    Added additional test files to test the balancing process.
    Removed memory leaks.

--- src/main.c
+++ src/main.c
@@ -9,12 +9,14 @@ struct args {
     int thread_id;
     pthread_barrier_t* mapstop; // Barrier that everyone syncs to
     int nr_files; // Number of files a mapper will handle
+    int nr_bytes; // Total size of the files the mapper has
     struct fileinfo* files; // The files a mapper will process
 };
 
 struct fileinfo {
     char fileName[MAX_BUFFER];
     int id;
+    int size;
     FILE* file;
 };
 
@@ -22,7 +24,7 @@ void printArgs(struct args myargs) {
     printf("thread_id %d; nr_files %d\n", myargs.thread_id, myargs.nr_files);
     if (myargs.nr_files > 0) {
         for (int i = 0; i < myargs.nr_files; i++) {
-            printf("file %d: id %d; filename %s;\n", i, myargs.files[i].id, myargs.files[i].fileName);
+            printf("File %d: id %d; filename %s; size %d\n", i, myargs.files[i].id, myargs.files[i].fileName, myargs.files[i].size);
         }
     }
 }
@@ -40,9 +42,9 @@ unsigned long fsize(char* file)
 void *mapper(void *arg) {
     struct args myargs = *(struct args *)arg;
 
-    printf("%d Mapper started\n", myargs.thread_id);
+    printf("Mapper %d started.\n", myargs.thread_id);
 
-    printf("%d Mapper has %d files.\n", myargs.thread_id, myargs.nr_files);
+    printf("Mapper %d has %d files.\n", myargs.thread_id, myargs.nr_files);
     printArgs(myargs);
 
     char wordBuffer[MAX_BUFFER];
@@ -50,10 +52,18 @@ void *mapper(void *arg) {
         for (int i = 0; i < myargs.nr_files; i++) {
             myargs.files[i].file = fopen(myargs.files[i].fileName, "r");
 
+            char *hehe = malloc(sizeof(char) * MAX_BUFFER * 10);
+            hehe[0] = '\0';
+            sprintf(hehe, "%d - ", myargs.thread_id);
+
             while (fscanf(myargs.files[i].file, "%s\n", wordBuffer) == 1) {
-                printf("%s ", wordBuffer);
+                strcat(hehe, wordBuffer);
+                strcat(hehe, " ");
+                // printf("%s ", wordBuffer);
             }
-            printf("\n");
+            printf("%s\n", hehe);
+
+            free(hehe);
 
             fclose(myargs.files[i].file);
         }
@@ -69,8 +79,37 @@ void *reducer(void *arg) {
     // Reducers wait until all mappers have finished.
     pthread_barrier_wait(myargs.mapstop);
 
-    printf("%d Reducer started\n", myargs.thread_id);
+    printf("Reducer %d started.\n", myargs.thread_id);
     return 0;
+}
+
+int compareSizeDesc(const void *a, const void *b) {
+    const struct fileinfo A = *(struct fileinfo *)a;
+    const struct fileinfo B = *(struct fileinfo *)b;
+    return (B.size - A.size);
+}
+
+void greedyPartition(struct fileinfo *files, int fileCount, int N, struct fileinfo **subsets, int *subsetSums, int *subsetCounts) {
+    for (int i = 0; i < N; i++) {
+        subsetSums[i] = 0;
+        subsetCounts[i] = 0;
+    }
+
+    // Descending order
+    qsort(files, fileCount, sizeof(struct fileinfo), compareSizeDesc);
+
+    for (int i = 0; i < fileCount; i++) {
+        int minSubset = 0;
+        for (int j = 1; j < N; j++) {
+            if (subsetSums[j] < subsetSums[minSubset]) {
+                minSubset = j;
+            }
+        }
+
+        subsets[minSubset][subsetCounts[minSubset]] = files[i];
+        subsetCounts[minSubset]++;
+        subsetSums[minSubset] += files[i].size;
+    }
 }
 
 int main(int argc, char **argv)
@@ -81,7 +120,7 @@ int main(int argc, char **argv)
     // Validate arguments
 
     if (argc < 4) {
-        printf("Correct usage:\n./tema1 [numar_mapperi] [numar_reduceri] [fisier_intrare]");
+        printf("Correct usage:\n./tema1 [numar_mapperi] [numar_reduceri] [fisier_intrare]\n");
         exit(1);
     }
 
@@ -126,16 +165,16 @@ int main(int argc, char **argv)
 
     for (int i = 0; i < NUM_THREADS; i++) {
         arguments[i].nr_files = 0;
+        arguments[i].nr_bytes = 0;
         arguments[i].files = NULL;
-
-        // fix this pls
-        arguments[i].files = malloc(sizeof(struct fileinfo) * nr_files);
     }
 
+    struct fileinfo files[nr_files];
+    int totalBytes = 0;
 
-    int i, r;
+    int r;
     char lineBuffer[MAX_BUFFER];
-    for (i = 0; i < nr_files; i++) {
+    for (int i = 0; i < nr_files; i++) {
         r = fscanf(input_file, "%s\n", lineBuffer);
 
         if (r == EOF) {
@@ -143,29 +182,57 @@ int main(int argc, char **argv)
             exit(1);
         }
 
+        totalBytes += fsize(lineBuffer);
         if (debug) {
             printf("File %s has size %lu.\n", lineBuffer, fsize(lineBuffer));
         }
 
         struct fileinfo newFile;
         strcpy(newFile.fileName, lineBuffer);
-        // newFile.file = fopen(lineBuffer, "r");
         newFile.id = i;
+        newFile.size = fsize(newFile.fileName);
 
-        // Even distribution, kinda bad -- should be done based on fsize
-        int lucky_mapper = i % nr_mappers;
-        arguments[lucky_mapper].files[arguments[lucky_mapper].nr_files] = newFile;
-        arguments[lucky_mapper].nr_files++;
+        files[i] = newFile;
     }
 
-    // arguments[0].files[0].id = 1;
-    // strcpy(arguments[0].files[0].fileName, "hehe");
-    // printArgs(arguments[0]);
+    fclose(input_file);
+
+    // Balance files for mapping
+
+    struct fileinfo **subsets = (struct fileinfo **)malloc(nr_mappers * sizeof(struct fileinfo *));
+    for (int i = 0; i < nr_mappers; i++) {
+        subsets[i] = (struct fileinfo *)malloc(nr_files * sizeof(struct fileinfo)); // Maximum files per subset
+    }
+
+    int subsetSums[nr_mappers];
+    int subsetCounts[nr_mappers];
+    greedyPartition(files, nr_files, nr_mappers, subsets, subsetSums, subsetCounts);
+
+    // Assign files in arguments
+
+    for (int i = 0; i < nr_mappers; i++) {
+        arguments[i].nr_files = subsetCounts[i];
+        arguments[i].nr_bytes = subsetSums[i];
+        arguments[i].files = subsets[i];
+
+        // Realloc down to size that is actually in use
+        arguments[i].files = realloc(arguments[i].files, arguments[i].nr_files * sizeof(struct fileinfo));
+    }
+
+    if (debug) {
+        for (int i = 0; i < nr_mappers; i++) {
+            printf("Subset %d (Total %d):\n", i + 1, subsetSums[i]);
+            for (int j = 0; j < subsetCounts[i]; j++) {
+                printf("- File: %s, id: %d, size: %d\n", subsets[i][j].fileName, subsets[i][j].id, subsets[i][j].size);
+            }
+            printf("\n");
+        }
+    }
 
     // Initialize threads
 
     // Note for self: last thread will be (NUM_THREADS - 1)
-	for (i = 0; i < NUM_THREADS; i++) {
+	for (int i = 0; i < NUM_THREADS; i++) {
 		arguments[i].thread_id = i;
         arguments[i].mapstop = &mapstop;
 
@@ -189,7 +256,7 @@ int main(int argc, char **argv)
     // Await threads
 
     void *status;
-    for (i = 0; i < NUM_THREADS; i++) {
+    for (int i = 0; i < NUM_THREADS; i++) {
 		r = pthread_join(threads[i], &status);
 
 		if (r) {
@@ -205,8 +272,19 @@ int main(int argc, char **argv)
 
     // Wrap-up (free & close)
 
+    for (int i = 0; i < NUM_THREADS; i++) {
+        if (i < nr_mappers) {
+            // Mappper arg freeing
+            free(arguments[i].files);
+        } else {
+            // Reducer arg freeing
+        }
+    }
+
     pthread_barrier_destroy(&mapstop);
-    fclose(input_file);
+
+    // Initial pointer not needed anymore
+    free(subsets);
 
     return 0;
 }
